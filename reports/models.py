169365a1041e8a41e7af01e6afcae84a406e249e
Tayan <w2116104@westminster.ac.uk>
2026-04-28 13:14:47 +0200

    Update models.py

--- reports/models.py
+++ reports/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-#Represents a yeam, storing info such as team name and the manager.
-
-class Team(models.Model):
-    team_name = models.CharField(max_length=100)
-    manager = models.CharField(max_length=100, null=True, blank=True)
-
-    def __str__(self):
-        return self.team_name
-    #String representation that is used within the admin panel.
+#Reports uses the shared skyapp.teams.models
