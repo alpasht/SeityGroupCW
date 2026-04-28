95a8fa174fcd276ff4ea89f50540c34cf4816bf7
Tayan <w2116104@westminster.ac.uk>
2026-04-28 13:11:16 +0200

    Update admin.py

--- skyapp/teams/admin.py
+++ skyapp/teams/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Team
 
-#REgisters the teamwithin django admin
+#Registers the teamwithin django admin
 
 admin.site.register(Team)
 
-# Register your models here.
+
